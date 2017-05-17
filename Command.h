774023b974f22dcc0474715ad1f820a4219e28ed
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2017-05-17 00:12:36 -0400

    (V0.97.1)Submitting a copy of the assignment to the student can now be switched on an of in the config file by the option called "CC_student" with values of "yes" and "no". If this option is commented out or does not exist in the config file, the student will be prompted to receive a copy of the submission with a yes or no option.

--- Command.h
+++ Command.h
@@ -2,7 +2,7 @@
 #define SICT_COMMAND_H_
 
 //#define SICT_DEBUG// uncomment to print the commands instead of executing them
-#define SICT_DEBUGEMAIL // uncomment to print the email command instead of executing it
+//#define SICT_DEBUGEMAIL // uncomment to print the email command instead of executing it
 #include <string>
 namespace sict{
 
