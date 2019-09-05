633278742ec416452aade046332f2a455f75b150
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2019-09-05 13:27:05 -0400

    renamed putenv back to what it was before

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main(int argc, char* argv[]){
   //set new shell environmental variable using putenv
   char lc_all[] = "LC_ALL=en_CA.UTF-8";
   char lc_col[] = "LC_COLLATE=en_CA.UTF-8";
-  _putenv(lc_all);
-  _putenv(lc_col);
+  putenv(lc_all);
+  putenv(lc_col);
   sict::Submitter S(argc, argv);
   return S.run();
 }
